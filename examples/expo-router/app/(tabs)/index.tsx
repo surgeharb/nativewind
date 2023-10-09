6761f4cdf299153078411c78e04656c65320d911
Serge Harb <me@sergeharb.com>
2023-10-09 11:21:08 +0200

    CSS error example

--- examples/expo-router/app/(tabs)/index.tsx
+++ examples/expo-router/app/(tabs)/index.tsx
@@ -9,6 +9,168 @@ const App = () => {
   return (
     <View className="flex-1 items-center justify-center" style={theme}>
       <Text className="text-[--theme-fg]">Try editing me! 🎉</Text>
+      <View>
+        <View className="gap-1" />
+        <View className="gap-2" />
+        <View className="gap-3" />
+        <View className="gap-4" />
+        <View className="gap-5" />
+        <View className="gap-6" />
+        <View className="gap-7" />
+        <View className="gap-8" />
+        <View className="gap-9" />
+        <View className="gap-10" />
+        <View className="gap-11" />
+        <View className="gap-12" />
+        <View className="gap-14" />
+        <View className="gap-16" />
+        <View className="gap-20" />
+        <View className="gap-22" />
+      </View>
+      <View>
+        <View className="gap-x-1" />
+        <View className="gap-x-2" />
+        <View className="gap-x-3" />
+        <View className="gap-x-4" />
+        <View className="gap-x-5" />
+        <View className="gap-x-6" />
+        <View className="gap-x-7" />
+        <View className="gap-x-8" />
+        <View className="gap-x-9" />
+        <View className="gap-x-10" />
+        <View className="gap-x-11" />
+        <View className="gap-x-12" />
+        <View className="gap-x-14" />
+        <View className="gap-x-16" />
+        <View className="gap-x-20" />
+        <View className="gap-x-22" />
+      </View>
+      <View>
+        <View className="gap-y-1" />
+        <View className="gap-y-2" />
+        <View className="gap-y-3" />
+        <View className="gap-y-4" />
+        <View className="gap-y-5" />
+        <View className="gap-y-6" />
+        <View className="gap-y-7" />
+        <View className="gap-y-8" />
+        <View className="gap-y-9" />
+        <View className="gap-y-10" />
+        <View className="gap-y-11" />
+        <View className="gap-y-12" />
+        <View className="gap-y-14" />
+        <View className="gap-y-16" />
+        <View className="gap-y-20" />
+        <View className="gap-y-22" />
+      </View>
+      <View>
+        <View className="p-1" />
+        <View className="p-2" />
+        <View className="p-3" />
+        <View className="p-4" />
+        <View className="p-5" />
+        <View className="p-6" />
+        <View className="p-7" />
+        <View className="p-8" />
+        <View className="p-9" />
+        <View className="p-10" />
+        <View className="p-11" />
+        <View className="p-12" />
+        <View className="p-14" />
+        <View className="p-16" />
+        <View className="p-20" />
+        <View className="p-22" />
+      </View>
+      <View>
+        <View className="px-1" />
+        <View className="px-2" />
+        <View className="px-3" />
+        <View className="px-4" />
+        <View className="px-5" />
+        <View className="px-6" />
+        <View className="px-7" />
+        <View className="px-8" />
+        <View className="px-9" />
+        <View className="px-10" />
+        <View className="px-11" />
+        <View className="px-12" />
+        <View className="px-14" />
+        <View className="px-16" />
+        <View className="px-20" />
+        <View className="px-22" />
+      </View>
+      <View>
+        <View className="py-1" />
+        <View className="py-2" />
+        <View className="py-3" />
+        <View className="py-4" />
+        <View className="py-5" />
+        <View className="py-6" />
+        <View className="py-7" />
+        <View className="py-8" />
+        <View className="py-9" />
+        <View className="py-10" />
+        <View className="py-11" />
+        <View className="py-12" />
+        <View className="py-14" />
+        <View className="py-16" />
+        <View className="py-20" />
+        <View className="py-22" />
+      </View>
+      <View>
+        <View className="m-1" />
+        <View className="m-2" />
+        <View className="m-3" />
+        <View className="m-4" />
+        <View className="m-5" />
+        <View className="m-6" />
+        <View className="m-7" />
+        <View className="m-8" />
+        <View className="m-9" />
+        <View className="m-10" />
+        <View className="m-11" />
+        <View className="m-12" />
+        <View className="m-14" />
+        <View className="m-16" />
+        <View className="m-20" />
+        <View className="m-22" />
+      </View>
+      <View>
+        <View className="mx-1" />
+        <View className="mx-2" />
+        <View className="mx-3" />
+        <View className="mx-4" />
+        <View className="mx-5" />
+        <View className="mx-6" />
+        <View className="mx-7" />
+        <View className="mx-8" />
+        <View className="mx-9" />
+        <View className="mx-10" />
+        <View className="mx-11" />
+        <View className="mx-12" />
+        <View className="mx-14" />
+        <View className="mx-16" />
+        <View className="mx-20" />
+        <View className="mx-22" />
+      </View>
+      <View>
+        <View className="my-1" />
+        <View className="my-2" />
+        <View className="my-3" />
+        <View className="my-4" />
+        <View className="my-5" />
+        <View className="my-6" />
+        <View className="my-7" />
+        <View className="my-8" />
+        <View className="my-9" />
+        <View className="my-10" />
+        <View className="my-11" />
+        <View className="my-12" />
+        <View className="my-14" />
+        <View className="my-16" />
+        <View className="my-20" />
+        <View className="my-22" />
+      </View>
     </View>
   );
 };
